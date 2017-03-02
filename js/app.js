e94443699c79eb0b7c586e917da59fc2967d5f45
Heather Nuffer <zzzuzz@gmail.com>
2017-03-01 22:35:31 -0700

    feat: Add playing against a dealer functionality

--- js/app.js
+++ js/app.js
@@ -5,37 +5,49 @@ function Card(myName, mySuit, myValue) {
 }
 
 // Setting up the deck.
-var deck = [];
-var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
-var names = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
-  "Nine", "Ten", "Jack", "Queen", "King"];
-
-for (var i = 0; i < suits.length; i++) {
-  for (var j = 0; j < names.length; j++) {
-    deck.push(new Card(names[j], suits[i], j+1));
+var cardDeck = {
+  allCards: [],
+  suits: ["Hearts", "Diamonds", "Spades", "Clubs"],
+  names: ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+  "Nine", "Ten", "Jack", "Queen", "King"],
+  usedCards: [],
+  populateDeck: function() {
+    for (var i = 0; i < this.suits.length; i++) {
+      for (var j = 0; j < this.names.length; j++) {
+        this.allCards.push(new Card(this.names[j], this.suits[i], j+1));
+      }
+    }
+    // The specifications indicate that aces should be always worth 11 instead
+    // of 1 in this version of the game. To change those values:
+    for (var i = 0; i < this.allCards.length; i++) {
+      if (this.allCards[i].name == "Ace") {
+        this.allCards[i].value += 10;
+      }
+    }
   }
-}
+};
 
-var usedCards = [];
+function Hand() {
+  this.cards = [];
+  this.currentTotal = 0;
 
-var hand = {
-  cards: [],
-  currentTotal: 0,
-  sumCardTotal: function() {
+  this.sumCardTotal = function() {
     this.currentTotal = 0;
     for (var i = 0; i < this.cards.length; i++) {
       this.currentTotal += this.cards[i].value;
     }
+  }
+
+  this.displayCardTotal = function() {
     $("#hdrTotal").html("Total value: " + this.currentTotal);
-    console.log("Total value of hand: " + this.currentTotal);
+  }
+
+  this.scoreHand = function() {
     if (this.currentTotal > 21) {
-      $("#btnStick").trigger("click");
       $("#hdrResult").html("BUST!");
     } else if (this.currentTotal == 21) {
-      $("#btnStick").trigger("click");
-      $("#hdrResult").html("Blackjack!");
+      $("#hdrResult").html("Blackjack! Winner!");
     } else if (this.currentTotal < 21 && this.cards.length == 5){
-      $("#btnStick").trigger("click");
       $("#hdrResult").html("5 card trick! Winner!");
     } else {
       //keep playing
@@ -43,47 +55,67 @@ var hand = {
   }
 };
 
-// Dealing cards
-function dealCards(num) {
-  for (var i = 0; i < num; i++) {
-    console.log("Time to deal a card");
-    hit();
+// Initiating a game
+
+function Game() {
+  this.usersHand = new Hand();
+  this.dealersHand = new Hand();
+  this.init = function() {
+    cardDeck.populateDeck();
   }
-}
+  this.hit = function(player) {
+    var goodCard = false;
+    do {
+      var index = getRandom(cardDeck.allCards.length);
+      if (!($.inArray(index, cardDeck.usedCards) > -1)) {
+        goodCard = true;
+        var card = cardDeck.allCards[index];
+        cardDeck.usedCards.push(index);
+        // If player is user, add to users' hand
+        if (player === "user") {
+          this.usersHand.cards.push(card);
+          var $myHandContent = $("<div>");
+          $myHandContent.addClass("current_hand").appendTo("#my_hand");
+          var imageUrl = "img/cards/" + card.suit + "/" + card.name + ".jpg";
+          $("<img>").appendTo($myHandContent).attr("src", imageUrl).fadeOut("slow")
+            .fadeIn("slow");
+        // If player is dealer, add to dealer's hand.
+        } else if (player == "dealer") {
+          this.dealersHand.cards.push(card);
+          this.dealersHand.sumCardTotal();
+        }
+      }
+    } while (!goodCard);
+    goodCard = false;
+    this.usersHand.sumCardTotal();
+    this.usersHand.displayCardTotal();
+    this.usersHand.scoreHand();
+  };
+  this.deal = function() {
+    this.hit('user');
+    this.hit('dealer');
+  };
+  this.stick = function() {
+    if (this.dealersHand.currentTotal > 21) {
+      $("#hdrResult").html("You win! The dealer went over with a score of " +
+        this.dealersHand.currentTotal + ".");
+    } else if (this.usersHand.currentTotal > 21) {
+      $("#hdrResult").html("You lose since you went over 21. Try again.");
+    } else if (this.usersHand.currentTotal > this.dealersHand.currentTotal) {
+      $("#hdrResult").html("Stick! Dealer's score is only " +
+        this.dealersHand.currentTotal +  ".So you are a winner!");
+    } else if (this.dealersHand.currentTotal === this.usersHand.currentTotal) {
+      $("#hdrResult").html("Tie!");
+    } else {
+      $("#hdrResult").html("You lose since the dealer got a higher score than you -- a " +
+      this.dealersHand.currentTotal + ".");
+    }
+  }
+};
+
+var game = new Game();
+game.init();
 
 function getRandom(num) {
   return Math.floor(Math.random() * num);
-}
-
-function hit() {
-  var goodCard = false;
-  do {
-    var index = getRandom(deck.length);
-    if (!($.inArray(index, usedCards) > -1)) {
-      goodCard = true;
-      var card = deck[index];
-      usedCards.push(index);
-      hand.cards.push(card);
-      console.log("usedCards = ", usedCards);
-      var $myHandContent = $("<div>");
-      $myHandContent.addClass("current_hand").appendTo("#my_hand");
-      var imageUrl = "img/cards/" + card.suit + "/" + card.name + ".jpg";
-      $("<img>").appendTo($myHandContent).attr("src", imageUrl).fadeOut("slow")
-        .fadeIn("slow");
-      hand.sumCardTotal();
-    }
-  } while (!goodCard);
-  goodCard = false;
-}
-
-function dealTwo() {
-  console.log("Time to deal");
-  dealCards(2);
-  $("#btnDeal").toggle();
-  $("#btnHit").toggle();
-  $("#btnStick").toggle();
-}
-
-function stick() {
-  $("#hdrResult").html("Stick!");
 }
